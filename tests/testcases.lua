1bcfb82ac46211690ed3416052cd1b991f8b31e3
Erutuon <5840197+Erutuon@users.noreply.github.com>
2024-01-27 20:00:33 -0600

    test simple example of CBOR sequence

--- tests/testcases.lua
+++ tests/testcases.lua
@@ -1,23 +1,24 @@
 function from_hex(hex)
-	return (hex:gsub('%x%x', function (x) return string.char(tonumber(x, 16)) end))
+	return (hex:gsub("%x%x", function (x) return string.char(tonumber(x, 16)) end))
 end
 
-local cbor = require 'cn-cbor'
+local cbor = require "cncbor"
 local function decode_from_hex(hex)
 	return cbor.decode(from_hex(hex))
 end
 
-print(require 'inspect'(decode_from_hex '9f018202039f0405ffff'))
-print(require 'inspect'(decode_from_hex '5F456279746573452074657874ff'))
+print(require "inspect" (decode_from_hex "9f018202039f0405ffff"))
 
-local data = assert(io.open('template_iter/m.cbor', 'rb')):read'a'
-local words = {}
-local ok, err = pcall(function()
-	for pos, template in cbor.iter(data) do
-		if template.parameters["1"] == "peo" then
-			table.insert(words, template.parameters["2"])
-		end
-	end
-end)
-if not ok then print(err) end
-print(table.concat(words, ", "))
+local cbor_text = from_hex("5F456279746573452074657874ff")
+print(require "inspect"(cbor.decode(cbor_text)))
+
+local repetitions = 2
+local cbor_sequence = cbor_text:rep(repetitions)
+
+local sequence = {}
+for i, val in cbor.iter(cbor_sequence) do
+    print("byte " .. i)
+    table.insert(sequence, val)
+end
+
+assert(#sequence == repetitions)
